c8bdaa2fdc789aa973c5675ef63bc9cb452ff088
100jobin <114425368+100jobin@users.noreply.github.com>
2025-06-14 17:58:23 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,3 +1,25 @@
+import appointment from './appointment-image.jpg'
+import author from './author-image.jpg'
+import book1 from './book1.jpeg'
+import book3 from './book3.jpg'
+import cardiology from './cardiology.png'
+import dermatology from './dermatology.png'
+import gynecology from './gynecology.png'
+import lab from './lab.jpg'
+import medicine from './medicine.jpg'
+import nephrology from './nephrology.png'
+import neurology from './neurology.png'
+import news from './news-image.jpg'
+import news1 from './news-image1.jpg'
+import news2 from './news-image2.jpg'
+import news3 from './news-image3.jpg'
+import oncology from './oncology.jpg'
+import slider1  from './slider1.jpg'
+import slider2 from './slider2.jpg'
+import slider3  from './slider3.jpg'
+import team1 from './team-image1.jpg'
+import team2 from './team-image2.jpg'
+import team3 from './team-image3.jpg'
 
 
 import logo from './logo.png'
@@ -21,6 +43,51 @@ import cross_icon from './cross_icon.png'
 
 export const assets = {
     
+ appointment , 
+
+ author , 
+
+ book1,  
+
+ book3 , 
+
+ cardiology , 
+
+ dermatology, 
+
+ gynecology , 
+
+lab , 
+
+ medicine, 
+
+nephrology , 
+
+ neurology , 
+
+ news , 
+
+ news1 , 
+
+ news2,  
+
+ news3 , 
+
+ oncology , 
+
+ slider1 ,  
+
+ slider2 , 
+
+ slider3,   
+
+ team1,  
+
+ team2, 
+
+ team3 ,
+
+
     logo,
     hero_img,
     cart_icon,
@@ -45,3 +112,4 @@ export const products = [
     
 
 ]
+
